43566675eccb104fa9673b0658d3674d268c6d6c
HubbeKing <hubbe128@gmail.com>
2020-02-17 23:27:29 +0200

    First try at associative array for PVC names

--- k8s-iscsi-backup.sh
+++ k8s-iscsi-backup.sh
@@ -4,8 +4,17 @@
 
 REPOSITORY=/path/to/borg/repo
 VOLUME_TYPE='kubernetes.io~iscsi'
+K8S_NAMESPACE='default'
 
+# exporting the passphrase means borg create/prune won't ask for it
 export BORG_PASSPHRASE='passphrase-goes-here'
+
+# first, build an associative array of volume name to PVC metadata name
+# get volume name to PVC metadata name from kubectl using jq
+JSON=$(kubectl --namespace=$K8S_NAMESPACE get pvc -o json | jq -r '{ (.items[].spec.volumeName): (.items[].metadata.name) }')
+# turn JSON object into bash 4+ associative array
+arrayAsString=$(echo $JSON | jq --raw-output '. | to_entries | map("[\(.key)]=\(.value)") | reduce .[] as $item ("associativeArray=("; . + $item + " ") + ")"')
+declare -A "$arrayAsString"
 
 if [[ ! -d $REPOSITORY ]]; then
     # initialize the borg repo if it doesn't exist
@@ -13,11 +22,15 @@ if [[ ! -d $REPOSITORY ]]; then
 fi
 
 for VOLUME in /var/lib/kubelet/pods/*/volumes/$VOLUME_TYPE/*; do
-    # the repository will have archives named backup_date-volume_name
-    # the root of each archive should be a folder with the volume name
+    # the repository will have archives named backup_date-pvc.metadata.name
+    # the root of each archive should be a folder containing all the volume data
     VOLUME_NAME=$(basename $VOLUME)
-    cd $VOLUME/..
-    borg create -v --stats --compression auto,zstd $REPOSITORY::{now:%Y-%m-%d}-$VOLUME_NAME $VOLUME_NAME
+    PVC_NAME=${associativeArray["$VOLUME_NAME"]}
+    if [ ! -z "$PVC_NAME" ]; then
+        # only try to create a backup if the volume appears to be an actual PVC we know about
+        cd $VOLUME/..
+        borg create -v --stats --compression auto,zstd $REPOSITORY::{now:%Y-%m-%d}-$PVC_NAME $VOLUME_NAME
+    fi
 done
 
 borg prune -v --list $REPOSITORY --keep-daily=7 --keep-weekly=4 --keep-monthly=6
